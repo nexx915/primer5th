d039ac4ba1e807d9f632561640fd7a6ab52de2ac
nexx <test@test.com>
2022-06-14 16:19:07 +0200

    adding more solutions

---
+++ e1.22/e1.22/e1.22.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item sum, item1;
+
+    while (std::cin >> item1) {
+        std::cout << item1 << std::endl;
+        sum += item1;
+        std::cout << "sum: " << sum << std::endl;
+    }
+    return 0;
+}
---
+++ e1.23/e1.23/e1.23.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item sum, item1;
+    int trans_amount = 0;
+
+    while (std::cin >> item1) {
+        trans_amount++;
+        sum += item1;
+        std::cout << "sum: " << sum << std::endl;
+        std::cout << "trans_amount: " << trans_amount << std::endl;
+    }
+        return 0;
+}
